7eae22aad3bd86630afdf354d85677eef2903517
Jawahar Suresh Babu <jawahars_16@live.in>
2020-06-16 18:05:58 +0530

    Added feedly subscription button

--- src/components/navigation-bar/index.jsx
+++ src/components/navigation-bar/index.jsx
@@ -25,16 +25,24 @@ const NavigationBar = props => {
   const isTalkPage = props.url.includes("talks")
   const isAboutPage = props.url.includes("about")
 
-  return <Box flexDirection='row' display='flex' justifyContent="center" marginBottom={-2}>
-    <Link to='/' className={isBlogPage ? styles.activeLink : styles.link}>
-      Blog
-    </Link>
-    <Link to='/talks' className={isTalkPage ? styles.activeLink : styles.link}>
-      Talks
-    </Link>
-    <Link to='/about' className={isAboutPage ? styles.activeLink : styles.link}>
-      About
-    </Link>
+  return <Box flexDirection='row' display='flex' justifyContent="space-between">
+    <Box flexDirection='row' display='flex' justifyContent="left" marginBottom={-2}>
+      <Link to='/' className={isBlogPage ? styles.activeLink : styles.link}>
+        Blog
+      </Link>
+      <Link to='/talks' className={isTalkPage ? styles.activeLink : styles.link}>
+        Talks
+      </Link>
+      <Link to='/about' className={isAboutPage ? styles.activeLink : styles.link}>
+        About
+      </Link>
+    </Box>
+
+    <div id='feedly'>
+      <a href='https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.jawahar.tech%2Frss.xml' target='blank'><img
+        id='feedlyFollow' src='http://s3.feedly.com/img/follows/feedly-follow-rectangle-volume-medium_2x.png'
+        alt='follow us in feedly' width='71' height='28'/></a>
+    </div>
   </Box>
 }
 
