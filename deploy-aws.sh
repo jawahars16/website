f7360895e2b3b498a995c14659f83c73b9dbed2b
Jawahar <jawahar@Jawahars-MacBook-Pro.local>
2021-08-22 16:30:26 +0530

    Added azure cert

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -4,5 +4,7 @@ set -x
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 npm install
+rm -rf .cache
+rm -rf public
 npm run build
 aws s3 sync public s3://jawahar.tech --delete
